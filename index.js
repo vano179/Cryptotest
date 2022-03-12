a881bd95aabf1db3407fd0b432ed0f9c1344c831
vano179 <101451845+vano179@users.noreply.github.com>
2022-03-12 11:59:13 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+{
+  "packages": [
+    "config/eslint-config-ironfish-react",
+    "config/eslint-config-ironfish",
+    "ironfish-cli",
+    "ironfish",
+    "ironfish-rust-nodejs",
+    "ironfish-rust-wasm/nodejs",
+    "ironfish-rust-wasm/web"
+  ],
+  "version": "independent",
+  "npmClient": "yarn",
+  "useWorkspaces": true
+}
